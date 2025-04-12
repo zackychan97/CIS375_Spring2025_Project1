6533add62479b5b1d9d6ba3ef5d1020fa9c47ea4
maniacmaniac19 <jsmith1088@gmail.com>
2025-04-12 10:28:28 -0500

    Added database connection, schema setup and added roles to register page.

---
+++ includes/db.php
@@ -0,0 +1,20 @@
+<?php
+//DON'T FORGET INLCUDES FOR DB
+
+//DATABASE CONNECTION
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'collab_db';
+
+
+//ESTABLISH CONNECTION
+$conn = mysqli_connect($host, $user, $password, $database);
+
+
+//CHECK CONNECTION
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        echo "Connected successfully";
+    }
